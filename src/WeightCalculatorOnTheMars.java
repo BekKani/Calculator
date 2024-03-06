19bb5910beb21e7873661ee17e93959a7eadd20d
kanykei <kbeksul1@stu.vistula.edu.pl>
2024-03-06 21:17:40 +0000

    Add class WeightCalculatorOnTheMars

---
+++ src/WeightCalculatorOnTheMars.java
@@ -0,0 +1,29 @@
+public class WeightCalculatorOnTheMars {
+        /* Variables declaration */
+        private static float earthWeightBeksultanova;
+        private static float gravityMars = 0.38f;
+
+        public static void main(String[] args) {
+            /* calculating weight on the Mars */
+            earthWeightBeksultanova= 56.69f;
+            System.out.println("Earth weight  Beksultanova: " +  earthWeightBeksultanova);
+
+            double marsWeightDouble =  earthWeightBeksultanova * gravityMars;
+            System.out.println("Mars weight (double): " + String.format("%.4f", marsWeightDouble));
+
+            /* converting double to int */
+            int marsWeightInt = (int) marsWeightDouble;
+            System.out.println("Mars weight (int): " + marsWeightInt);
+
+             /*converting int to char*/
+            char marsWeightChar = (char) marsWeightInt;
+            System.out.println("Mars weight (char): " + marsWeightChar);
+
+            /* performing some math operation */
+            int modifiedMarsWeightInt = marsWeightChar + 70;
+            System.out.println("Modified Mars weight (int): " + modifiedMarsWeightInt);
+        }
+    }
+
+
+
