026fbf25cd92bf689e8c5966c1aab18113e1318a
kanykei <kbeksul1@stu.vistula.edu.pl>
2024-03-06 22:37:38 +0000

    Add class EvenOrNotEven

--- src/EvenOrNotEven.java
+++ src/EvenOrNotEven.java
@@ -8,6 +8,7 @@ public class EvenOrNotEven {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("write your number from 1 to 100");
+        // use scaner for recognizing numbers
         int number =scanner.nextInt();
 
               if (number % 2==0) {
---
+++ src/task4.java
@@ -0,0 +1,6 @@
+public class task4 {
+    private static int amountOfBricks=67;
+    private static int containerCapacity=8;
+    
+
+}
