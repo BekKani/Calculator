43c7ca6891a33866e4d5c9663e3ea9287568f75c
kanykei <kbeksul1@stu.vistula.edu.pl>
2024-03-06 22:22:21 +0000

    Add class EvenOrNotEven

---
+++ src/EvenOrNotEven.java
@@ -0,0 +1,22 @@
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class EvenOrNotEven {
+
+    //Variables declaration
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("write your number from 1 to 100");
+        int number =scanner.nextInt();
+
+              if (number % 2==0) {
+                  System.out.println(" Your number is Even");
+              }
+              else {
+                  System.out.println("Your number is Odd");
+              }
+
+
+    }
+}
