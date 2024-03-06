2a6869d442c6d79188922a413332023611464974
kanykei <kbeksul1@stu.vistula.edu.pl>
2024-03-06 23:01:15 +0000

    Add class task4

--- src/task4.java
+++ src/task4.java
@@ -1,6 +1,15 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class task4 {
-    private static int amountOfBricks=67;
-    private static int containerCapacity=8;
-    
+    //choose odd number between 50 and 100
+    private static int amountOfBricks = 67;
+    // choose even number between 5 and 10
+    private static int containerCapacity = 8;
+    // calculating how many full containers we have
+
+
+    public static void main(String... args){
+
 
 }
+  }
